03ad2800b9f959ba49baccad5d8ffdb6a9a6e5f6
evivse <ya.e-se@ya.ru>
2016-10-17 20:45:56 +0300

    Создал рамку из вертикальных и горизонтальных линий

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,6 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25);
+
+            //отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine( 0, 78, 0, '+' );
+            HorizontalLine downLine = new HorizontalLine( 0, 78, 24, '+' );
+            VerticalLine leftLine = new VerticalLine( 0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine( 0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
