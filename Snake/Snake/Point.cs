57eb0013e4216e343c0c58fe67e254a2b9bc2975
evivse <ya.e-se@ya.ru>
2016-10-21 19:50:13 +0300

    управляемая змейка

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -50,6 +50,11 @@ namespace Snake
             }
         }
 
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
         internal void Clear()
         {
             sym = ' ';
